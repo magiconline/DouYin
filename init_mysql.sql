ea7082bd9f732adcdc0aaffdab720484b2962179
axingxing <13730772627@163.com>
2022-05-10 20:33:03 +0800

    update

--- init_mysql.sql
+++ init_mysql.sql
@@ -14,3 +14,12 @@ CREATE TABLE `video`(
 	`upload_time` INT UNSIGNED NOT NULL,
 	PRIMARY KEY (`video_id`)
 );
+
+CREATE TABLE `user` (
+  `user_id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `user_name` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(45) NOT NULL,
+  `follow_count` INT UNSIGNED NOT NULL,
+  `follower_count` INT UNSIGNED NOT NULL,
+  `token` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`user_id`));
