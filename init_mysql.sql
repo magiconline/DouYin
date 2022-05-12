d2d9d0b2a4f75fcb7d32b1c03b3c4132a5ea5c39
Chi Zhang <chizhang0513@qq.com>
2022-05-12 17:08:55 +0800

    video表字段添加unique约束

--- init_mysql.sql
+++ init_mysql.sql
@@ -7,8 +7,8 @@ DROP TABLE IF EXISTS `video`;
 CREATE TABLE `video`(
 	`video_id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
 	`user_id` BIGINT UNSIGNED NOT NULL,
-	`play_url` TEXT NOT NULL,
-	`cover_url` TEXT NOT NULL,
+	`play_url` TEXT NOT NULL UNIQUE,
+	`cover_url` TEXT NOT NULL UNIQUE,
 	`favorite_count` INT UNSIGNED NOT NULL,
 	`comment_count` INT UNSIGNED NOT NULL,
 	`upload_time` INT UNSIGNED NOT NULL,
