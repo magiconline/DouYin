8d9975d419baac2c8af2a747036f1fc00b80a610
Chi Zhang <chizhang0513@qq.com>
2022-05-16 20:50:36 +0800

    删除测试code

--- server.go
+++ server.go
@@ -26,15 +26,6 @@ func main() {
 		os.Exit(1)
 	}
 
-	// test
-	// videoTable := repository.VideoTable{2, 0, "test", "test", 0, 0, 0}
-	// err = repository.InsertVideoTable(&videoTable)
-	// if err != nil {
-	// 	logger.Logger.Println("数据插入失败,", err)
-	// } else {
-	// 	logger.Logger.Println("数据插入成功")
-	// }
-
 	r := gin.Default()
 
 	r.GET("/douyin/feed/", func(ctx *gin.Context) {
