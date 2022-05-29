7c73e24629977f4a345adf70d1b4793ece4995b9
axingxing <13730772627@163.com>
2022-05-29 15:48:56 +0800

    apptest

--- repository/user.go
+++ repository/user.go
@@ -1,11 +1,11 @@
 package repository
 
 // "DouYin/json"
+import "strconv"
 
 type User struct {
 	// gorm.Model
 	UserId        int64  `gorm:"column:user_id; primary_key; AUTO_INCREMENT"`
-	Email         string `gorm:"column:email"`
 	UserName      string `gorm:"column:user_name"`
 	Password      string `gorm:"column:password"`
 	FollowCount   int    `gorm:"column:follow_count"`
@@ -13,33 +13,47 @@ type User struct {
 }
 
 //根据email pwd查找用户
-func FindUserbyEmailandPwd(email, pwd string) (*User, error) {
+// func FindUserbyEmailandPwd(email, pwd string) (*User, error) {
+// 	var user User
+// 	err := DB.Table("user").Where("email = ? and password = ?", email, pwd).First(&user).Error
+// 	return &user, err
+// }
+
+func FindUserbyNameandPwd(name, pwd string) (*User, error) {
 	var user User
-	err := DB.Table("user").Where("email = ? and password = ?", email, pwd).First(&user).Error
+	err := DB.Table("user").Where("user_name = ? and password = ?", name, pwd).First(&user).Error
 	return &user, err
 }
 
 //根据email查找用户
-func FindUserbyEmail(email string) (*User, error) {
+// func FindUserbyEmail(email string) (*User, error) {
+// 	var user User
+// 	err := DB.Table("user").Where("email = ? ", email).First(&user).Error
+// 	return &user, err
+// }
+
+func FindUserbyName(name string) (*User, error) {
 	var user User
-	err := DB.Table("user").Where("email = ? ", email).First(&user).Error
+	err := DB.Table("user").Where("user_name = ? ", name).First(&user).Error
 	return &user, err
 }
 
 //根据userid查找用户
 func FindUserbyID(userid string) (*User, error) {
 	var user User
-	err := DB.Table("user").Where("user_id = ?", userid).First(&user).Error
+	// fmt.Println("string = ", userid)
+	id, _ := strconv.Atoi(userid)
+	// fmt.Println("int = ", id)
+	err := DB.Table("user").Where("user_id = ?", id).First(&user).Error
 	return &user, err
 }
 
 // 创建用户
-func CreateUser(email, pwd string) (*User, error) {
+func CreateUser(username, pwd string) (*User, error) {
 	// _, err1 := FindUserbyEmail(email)
 	// if err1 == nil {
 	user := User{
-		Email:    email,
-		UserName: "user_" + email,
+		UserName: username,
 		Password: pwd,
 	}
 	err := DB.Table("user").Create(&user).Error
