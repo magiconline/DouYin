f44e705fcb989678d6a034ba9ab0f399c35ca281
axingxing <13730772627@163.com>
2022-05-29 13:35:20 +0800

    json

--- controller/user.go
+++ controller/user.go
@@ -21,21 +21,21 @@ func UserLogin(c *gin.Context) *gin.H {
 	_, err1 := repository.FindUserbyEmail(email)
 	if err1 == gorm.ErrRecordNotFound {
 		return &gin.H{
-			"code": 1,
-			"msg":  "用户邮箱不存在,请注册",
+			"status_code": 1,
+			"status_msg":  "用户邮箱不存在,请注册",
 		}
 	} else if err1 == nil {
 		user, err := repository.FindUserbyEmailandPwd(email, pwd)
 		//如果数据库查不到或者密码错误
 		if err == gorm.ErrRecordNotFound {
 			return &gin.H{
-				"code": 1,
-				"msg":  "用户密码错误",
+				"status_code": 1,
+				"status_msg":  "用户密码错误",
 			}
 		} else if err != nil {
 			return &gin.H{
-				"code": 1,
-				"msg":  "数据库查询出错，请重新登录",
+				"status_code": 1,
+				"status_msg":  "数据库查询出错，请重新登录",
 			}
 		}
 
@@ -43,22 +43,22 @@ func UserLogin(c *gin.Context) *gin.H {
 		token, err := service.GenerateToken(uint(user.UserId))
 		if err != nil {
 			return &gin.H{
-				"code": 1,
-				"msg":  "token生成失败请重新登录",
+				"status_code": 1,
+				"status_msg":  "token生成失败请重新登录",
 			}
 		}
 
 		//查到了就返回json
 		return &gin.H{
-			"code":    0,
-			"msg":     "SUCCESS",
-			"user_id": user.UserId,
-			"token":   token,
+			"status_code": 0,
+			"status_msg":  "SUCCESS",
+			"user_id":     user.UserId,
+			"token":       token,
 		}
 	}
 	return &gin.H{
-		"code": 1,
-		"msg":  "数据库查询出错",
+		"status_code": 1,
+		"status_msg":  "数据库查询出错",
 	}
 }
 
@@ -74,41 +74,41 @@ func UserRegister(c *gin.Context) *gin.H {
 		//若创建新用户不成功
 		if err != nil {
 			return &gin.H{
-				"code": 1,
-				"msg":  "数据库创建用户操作错误",
+				"status_code": 1,
+				"status_msg":  "数据库创建用户操作错误",
 			}
 		}
 		token, err := service.GenerateToken(uint(u.UserId))
 		if err != nil {
 			return &gin.H{
-				"code": 1,
-				"msg":  "token生成失败请重新登录",
+				"status_code": 1,
+				"status_msg":  "token生成失败请重新登录",
 			}
 		}
 		return &gin.H{
-			"code":    0,
-			"msg":     "success",
-			"user_id": u.UserId,
-			"token":   token,
+			"status_code": 0,
+			"status_msg":  "success",
+			"user_id":     u.UserId,
+			"token":       token,
 		}
 	} else {
 		//若查找返回的err不是不存在而是别的
 		if err1 != nil {
 			return &gin.H{
-				"code": 1,
-				"msg":  "数据库查询用户操作错误",
+				"status_code": 1,
+				"status_msg":  "数据库查询用户操作错误",
 			}
 		}
 		if err1 == nil {
 			return &gin.H{
-				"code": 1,
-				"msg":  "该邮箱已被注册",
+				"status_code": 1,
+				"status_msg":  "该邮箱已被注册",
 			}
 		}
 	}
 	return &gin.H{
-		"code": 1,
-		"msg":  "注册失败",
+		"status_code": 1,
+		"status_msg":  "注册失败",
 	}
 
 }
@@ -119,32 +119,34 @@ func UserInfo(c *gin.Context) *gin.H {
 	user, err := repository.FindUserbyID(userid)
 	if err == gorm.ErrRecordNotFound {
 		return &gin.H{
-			"code": 1,
-			"msg":  "用户不存在",
+			"status_code": 1,
+			"status_msg":  "用户不存在",
 		}
 	}
 
 	if token == "" {
 		return &gin.H{
-			"code": 1,
-			"msg":  "token不存在",
+			"status_code": 1,
+			"status_msg":  "token不存在",
 		}
 	}
 
 	_, err = service.ParseToken(token)
 	if err != nil {
 		return &gin.H{
-			"code": 1,
-			"msg":  "token解析失败",
+			"status_code": 1,
+			"status_msg":  "token解析失败",
 		}
 	}
 
 	return &gin.H{
-		"code":           0,
-		"msg":            "sucess",
-		"id":             user.UserId,
-		"name":           user.UserName,
-		"follow_count":   user.FollowCount,
-		"follower_count": user.FollowerCount,
+		"status_code": 0,
+		"status_msg":  "sucess",
+		"user": gin.H{
+			"id":             user.UserId,
+			"name":           user.UserName,
+			"follow_count":   user.FollowCount,
+			"follower_count": user.FollowerCount,
+		},
 	}
 }
