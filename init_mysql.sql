44d5b921d6aee42f2e2bfae69bf2d51e1cb69ea4
Chi Zhang <chizhang0513@qq.com>
2022-05-29 16:36:07 +0800

    修改测试数据

--- init_mysql.sql
+++ init_mysql.sql
@@ -41,6 +41,9 @@ INSERT INTO `users` (`id`, `user_name`, `password`, `follow_count`, `follower_co
 -- 插入点赞状态表
 INSERT INTO douyin.star (user_id, video_id) VALUES (1, 1);
 -- 插入视频
-INSERT INTO `video` (`video_id`, `user_id`, `play_url`, `cover_url`, `favorite_count`, `comment_count`, `upload_time`) VALUES (1, 1, "/static/2022/05/16/抖音-记录美好生活.mp4", "/static/2022/05/16/抖音-记录美好生活.jpg", 0, 0, UNIX_TIMESTAMP());
-INSERT INTO `video` (`video_id`, `user_id`, `play_url`, `cover_url`, `favorite_count`, `comment_count`, `upload_time`) VALUES (2, 1, "/static/2022/05/16/抖音-记录美好生活(1).mp4", "/static/2022/05/16/抖音-记录美好生活.jpg", 0, 0, UNIX_TIMESTAMP());
-INSERT INTO `video` (`video_id`, `user_id`, `play_url`, `cover_url`, `favorite_count`, `comment_count`, `upload_time`) VALUES (3, 1, "/static/2022/05/16/抖音-记录美好生活(2).mp4", "/static/2022/05/16/抖音-记录美好生活.jpg", 0, 0, UNIX_TIMESTAMP());
+INSERT INTO `video` (`video_id`, `user_id`, `play_url`, `cover_url`, `favorite_count`, `comment_count`, `upload_time`, `title`)
+ VALUES (1, 1, "/static/2022/05/16/抖音-记录美好生活.mp4", "/static/2022/05/16/抖音-记录美好生活.jpg", 0, 0, UNIX_TIMESTAMP(), "测试title");
+INSERT INTO `video` (`video_id`, `user_id`, `play_url`, `cover_url`, `favorite_count`, `comment_count`, `upload_time`, `title`) 
+VALUES (2, 1, "/static/2022/05/16/抖音-记录美好生活(1).mp4", "/static/2022/05/16/抖音-记录美好生活.jpg", 0, 0, UNIX_TIMESTAMP(), "测试title");
+INSERT INTO `video` (`video_id`, `user_id`, `play_url`, `cover_url`, `favorite_count`, `comment_count`, `upload_time`, `title`) 
+VALUES (3, 1, "/static/2022/05/16/抖音-记录美好生活(2).mp4", "/static/2022/05/16/抖音-记录美好生活.jpg", 0, 0, UNIX_TIMESTAMP(), "测试title");
