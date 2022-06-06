a8b26fcc7d7300d3bfda11bd03f0b8ed3ed2814f
Chi Zhang <chizhang0513@qq.com>
2022-06-06 16:38:59 +0800

    配置redis

--- repository/init_db.go
+++ repository/init_db.go
@@ -1,6 +1,9 @@
 package repository
 
 import (
+	"context"
+
+	"github.com/go-redis/redis/v9"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -8,8 +11,12 @@ import (
 var (
 	mysqlUrl = "root:123456@tcp(127.0.0.1:3306)/douyin"
 	DB       *gorm.DB
+	RDB      *redis.Client
+	CTX      = context.Background()
 )
 
+// 初始化mysql
+// 初始化redis
 func Init() error {
 	var err error
 	DB, err = gorm.Open(mysql.Open(mysqlUrl), &gorm.Config{})
@@ -19,5 +26,11 @@ func Init() error {
 	DB.AutoMigrate(&Star{})
 	DB.AutoMigrate(&User{})
 	DB.AutoMigrate(&Relation{})
+
+	RDB = redis.NewClient(&redis.Options{
+		Addr:     "127.0.0.1:6379",
+		Password: "",
+		DB:       0,
+	})
 	return nil
 }
