53ab31095e975c726075c4f7a279bf2602e8b73f
Chi Zhang <chizhang0513@qq.com>
2022-06-11 03:28:15 +0800

    修改服务器初始化方式

--- logger/logger.go
+++ logger/logger.go
@@ -7,12 +7,17 @@ import (
 
 var Logger log.Logger
 
+var Println = Logger.Println
+var Fatalln = Logger.Fatalln
+
 func Init(logFilePath string) error {
+	// 0644: 用户具有读写权限，组用户和其它用户具有只读权限；
 	logFile, err := os.OpenFile(logFilePath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
 	if err != nil {
 		return err
 	}
-	Logger = log.Logger{}
-	Logger.SetOutput(logFile)
+
+	Logger = *log.New(logFile, "", log.Ldate|log.Ltime)
+
 	return nil
 }
